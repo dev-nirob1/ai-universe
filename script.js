48a6c8699a76412238e6e6f3bfce55369c379adc
dev-nirob1 <dev-nirob1@gmail.com>
2023-03-04 02:08:29 +0600

    card slice, card features, card image, card, name added

--- script.js
+++ script.js
@@ -3,21 +3,22 @@ const fetchCategories = () => {
 }
 const showCardItems = cards => {
     const cardContainer = document.getElementById('card-items');
+    cards = cards.slice(0, 6);
     cards.forEach(card => {
         const cardDiv = document.createElement('div');
         cardDiv.classList.add("card", "p-3", "border", "rounded-lg");
-        cardDiv.innerHTML = ` <img src="" class="rounded-lg h-56" alt="card logo">
+        cardDiv.innerHTML = ` <img src="${card.image}" class="rounded-lg h-56" alt="card logo">
         <h3 class="text-2xl font-semibold">Features</h3>
         <ol class="list-decimal p-5">
-            <li>1</li>
-            <li>2</li>
-            <li>3</li>
+            <li>${card.features[0]}</li>
+            <li>${card.features[1]}</li>
+            <li>${card.features[2]}</li>
         </ol>
         <hr class="my-5">
         <div class="flex justify-between items-center">
             <div>
-                <h4 class="text-2xl font-semibold">name</h4>
-                <p><i class="fa-solid fa-calendar-days"></i> <span></span></p>
+                <h4 class="text-2xl font-semibold">${card.name}</h4>
+                <p><i class="fa-solid fa-calendar-days"></i> <span>${card.published_in}</span></p>
             </div>
             <button class="bg-red-300 text-red-600 rounded-full p-3 text-xl"><i
              class="fa-solid fa-arrow-right"></i></button>`
