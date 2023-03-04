56ba0f6c67c4d507f76b423d96fb5af84fc818e3
dev-nirob1 <dev-nirob1@gmail.com>
2023-03-04 19:14:25 +0600

    modal added

--- script.js
+++ script.js
@@ -2,16 +2,20 @@ const fetchCategories = () => {
     fetch('https://openapi.programming-hero.com/api/ai/tools').then(res => res.json()).then(data => showCardItems(data.data.tools))
 }
 
+// card-items and see-more-btn 
+
 const showCardItems = cards => {
-    const cardContainer = document.getElementById('card-container');
+
     const seeMoreButton = document.getElementById('see-more-btn')
-    if(cards.length > 6){
+
+    if (cards.length > 6) {
         cards = cards.slice(0, 6);
         seeMoreButton.classList.remove('hidden');
     }
-    else{
+    else {
         seeMoreButton.classList.add('hidden')
     }
+    const cardContainer = document.getElementById('card-container');
 
     cards.forEach(card => {
         const cardDiv = document.createElement('div');
@@ -19,9 +23,9 @@ const showCardItems = cards => {
         cardDiv.innerHTML = ` <img src="${card.image}" class="rounded-lg h-56" alt="card logo">
         <h3 class="text-2xl font-semibold">Features</h3>
         <ol class="list-decimal p-5">
-        <li>${card.features[0]}</li>
-        <li>${card.features[1]}</li>
-        <li>${card.features[2]}</li>
+        ${card.features[0] ? `<li>${card.features[0]}</li>` : ''}
+        ${card.features[1] ? `<li>${card.features[1]}</li>` : ''}
+        ${card.features[2] ? `<li>${card.features[2]}</li>` : ''}
         </ol>
         <hr class="my-5">
         <div class="flex justify-between items-center">
@@ -29,26 +33,24 @@ const showCardItems = cards => {
         <h4 class="text-2xl font-semibold">${card.name}</h4>
                 <p><i class="fa-solid fa-calendar-days"></i> <span>${card.published_in}</span></p>
                 </div>
-                <button class="bg-red-300 text-red-600 rounded-full p-3 text-xl"><i
-                class="fa-solid fa-arrow-right"></i></button>`
-                cardContainer.appendChild(cardDiv)
-                
+            <label class="rounded-full px-4 py-3 bg-red-300 text-red-500" for="my-modal-5"> <i class="fa-solid fa-arrow-right"></i></label>
+                `
+
+        cardContainer.appendChild(cardDiv)
+
         loaderSection(false);
     })
 }
 
-
+// loader added here 
 const loaderSection = isLoading => {
     const loader = document.getElementById('loader');
-    if(isLoading){
+    if (isLoading) {
         loader.classList.remove('hidden')
     }
-    else{
+    else {
         loader.classList.add('hidden')
     }
 }
-// document.getElementById('see-more-btn').addEventListener('click', function(){
-
-// })
 
 fetchCategories()
